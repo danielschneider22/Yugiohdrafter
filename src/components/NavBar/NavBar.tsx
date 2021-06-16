afdbe9727d0ecd6e6a44d456518418a2640e02f9
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-06-15 21:59:38 -0400

    more css improvements

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -6,12 +6,12 @@ function NavBar(props: ParentProps) {
     return (
         <div className="pos-f-t">
             <div className="collapse" id="navbarToggleExternalContent">
-                <div className="bg-dark p-4">
+                <div className="bg-primary p-4">
                     <h4 className="text-white">Collapsed content</h4>
                     <span className="text-muted">Toggleable via the navbar brand.</span>
                 </div>
             </div>
-            <nav className="navbar navbar-dark bg-dark">
+            <nav className="navbar navbar-dark bg-primary">
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
