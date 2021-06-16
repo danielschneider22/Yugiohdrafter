84172df8e4f9e3108b4045ad412e5551fe0cbac2
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-06-15 22:58:46 -0400

    remove booster button

--- src/components/BoosterPicker/BoosterSelect.tsx
+++ src/components/BoosterPicker/BoosterSelect.tsx
@@ -1,5 +1,7 @@
+import { useDispatch } from "react-redux";
 import { Booster } from "../../constants/Booster";
 import { CardSet } from "../../constants/CardSet";
+import { removeBooster } from "../../data/boosters/actions";
 
 interface ParentProps {
     cardSets: CardSet[]
@@ -10,9 +12,15 @@ interface ParentProps {
 
 function BoosterSelect(props: ParentProps) {
   const {cardSets, boosterNum, boosterChanged, booster} = props;
+  const dispatch = useDispatch();
+
+  function dispatchRemoveBooster() {
+    dispatch(removeBooster(booster.id))
+  }
 
   return (
       <div className="BoosterSelect d-flex m-1 p-0">
+          <div className="DeleteButton btn-sm btn-danger" onClick={dispatchRemoveBooster}>x</div>
           <div className={"BoosterText"}>Booster #{(boosterNum + 1) + ": "}</div>
           <select value={booster.cardSetName} onChange={(event) => boosterChanged(booster.id, event.target.value)}>
             {cardSets.map((set) => {
