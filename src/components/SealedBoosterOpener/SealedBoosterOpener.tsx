b4b2938734542c8c976f0c0bac088d0ed5f3161a
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-06-18 11:05:13 -0400

    remove dup code

--- src/components/SealedBoosterOpener/SealedBoosterOpener.tsx
+++ src/components/SealedBoosterOpener/SealedBoosterOpener.tsx
@@ -1,16 +1,15 @@
+import './SealedBoosterOpener.css';
+
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+
 import { Card } from '../../constants/Card';
 import { resetBoosterCards, updateBooster } from '../../data/boosters/actions';
 import { getBoosters } from '../../data/boosters/selectors';
-import { addCards } from '../../data/cards/actions';
-import { fetchCards } from '../../data/cards/operations';
 import { getCardsById } from '../../data/cards/selectors';
-import { updateCardIds } from '../../data/cardSets/actions';
 import { getCardSetsById } from '../../data/cardSets/selectors';
 import NavBar from '../NavBar/NavBar';
 import { createBooster } from './BoosterCreatorHelper';
-import './SealedBoosterOpener.css';
 
 interface ParentProps{
   changePage: React.Dispatch<React.SetStateAction<string>>
@@ -29,22 +28,6 @@ function SealedBoosterOpener(props: ParentProps) {
     if(booster.cardIds)
       cards.push(...booster.cardIds.map((card_id) => cardsById[card_id])!.filter((card) => !!card))
   })
-
-  useEffect(() => {
-    const fetchedSets = {} as {[key: string]: string}
-    Object.values(boosters).forEach((booster) => {
-      if(!fetchedSets[booster.cardSetName]) {
-        const cardsOfSet = localStorage.getItem(booster.cardSetName);
-        if(cardsOfSet) {
-          dispatch(addCards(JSON.parse(cardsOfSet) as Card[]))
-          dispatch(updateCardIds(JSON.parse(cardsOfSet) as Card[], booster.cardSetName))
-        } else {
-          fetchCards(dispatch, booster.cardSetName);
-        }
-        fetchedSets[booster.cardSetName] = booster.cardSetName
-      }
-    })
-  }, [dispatch, boosters]);
 
   useEffect(() => {
     Object.values(boosters).forEach((booster) => {
