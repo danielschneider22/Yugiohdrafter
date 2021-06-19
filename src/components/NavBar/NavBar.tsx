63666dbe1de1a38285095ac0ee0e169baab971a9
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-06-19 15:58:04 -0400

    better nav bar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -11,10 +11,21 @@ function NavBar(props: ParentProps) {
                     <span className="text-muted">Toggleable via the navbar brand.</span>
                 </div>
             </div>
-            <nav className="navbar navbar-dark bg-primary">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+                {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
-                </button>
+                </button> */}
+                <div className="collapse navbar-collapse" style={{marginLeft: "5px"}} id="navbarTogglerDemo01">
+                    <a className="navbar-brand" href="#">Yugioh Drafter</a>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Join Draft Room</a>
+                    </li>
+                    </ul>
+                </div>
             </nav>
         </div>
 
