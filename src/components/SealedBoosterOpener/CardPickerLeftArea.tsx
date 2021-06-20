2d7c02f79295ae3e28c164bb78f4a45a260dc734
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-06-20 14:28:51 -0400

    smallchanges

--- src/components/SealedBoosterOpener/CardPickerLeftArea.tsx
+++ src/components/SealedBoosterOpener/CardPickerLeftArea.tsx
@@ -16,16 +16,18 @@ function CardPickerLeftArea(props: ParentProps) {
   if(parentWidth !== 0) {
     parentMaxWidth = parentWidth
   }
-  const mainDeckStyle = showSidebar ? {left: parentMaxWidth - 86} : {left: "-35px"}
+  const tabsStyle = showSidebar ? {left: parentMaxWidth - 86} : {left: "-35px"}
   return (
     <div className={"CardPickerLeftArea active clearfix"}>
-      <div>
-        <div onClick={toggleSidebar} className={"sideboard-button"} style={mainDeckStyle}>Main Deck</div>
+      {parentMaxWidth && <div className="CardPickerButtonContainer">
+        <div onClick={toggleSidebar} className={"CardPickerTab MainDeck"} style={tabsStyle}>Main Deck</div>
+        <div onClick={toggleSidebar} className={"CardPickerTab ExtraDeck"} style={tabsStyle}>Extra Deck</div>
       </div>
+      }
       
       <div className="CardsWrapper">
         {cards && showSidebar && cards.map((card, idx) => {
-          return <img className="CardLeftArea" key={card.name + idx} alt={card.name} src={card.card_images[0].image_url} width={"150"} height={"219"}/>
+          return <img className="CardLeftArea" key={card.name + idx} alt={card.name} src={card.card_images[0].image_url} width={"250"} height={"320"}/>
         })}
       </div>
     </div> 
