c7eaa789787b1c8fc7c03ef6b0eea87aa7087744
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-07-04 15:22:44 -0400

    css and timing

--- src/App.tsx
+++ src/App.tsx
@@ -28,7 +28,7 @@ function App() {
       <ToastManager
         position="bottom-left"
         autoDelete={true}
-        dismissTime={3000}
+        dismissTime={5000}
       />
       <div className={"AppWrapper maxWH"}>
         <Router>
