e602218ac499cf88a4b2221a115548990c836549
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-07-04 10:29:32 -0400

    css changes

--- src/components/CustomSetPopup/CustomSetPopup.tsx
+++ src/components/CustomSetPopup/CustomSetPopup.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './CustomSetPopup.css';
 
 interface ParentProps{
@@ -10,6 +11,9 @@ function CustomSetPopup(props: ParentProps) {
 Dark Magician
 Blue-Eyes White Dragon
   `;
+
+    const [setName, setSetName] = useState("")
+    const [cardList, setCardList] = useState("")
     return (
         <div className="container-fluid container-xl d-flex align-items-center justify-content-between">
             <nav id="navbar" className="navbar navbar-mobile customSet">
@@ -18,19 +22,19 @@ Blue-Eyes White Dragon
                         <div className="row h-100">
                             <div className="col-lg-12">
                                 <div className="contact-form h-100">
-                                    <div className="InfoBlurb">
+                                    <div className="CreateCustomSetTitle">
                                         Create a Custom Set
                                     </div>
                                     <div className="form-group setName">
-                                        <label className="control-label col-6" htmlFor="fname">Set Name:</label>
+                                        <label className="control-label col-6" htmlFor="setName">Set Name:</label>
                                         <div className="col-6">          
-                                            <input type="text" className="form-control" id="fname" placeholder="Enter Set Name" name="fname"/>
+                                            <input value={setName} type="text" className="form-control" placeholder="Enter Set Name" name="setName"/>
                                         </div>
                                     </div>
                                     <div className="form-group CreateSetTextArea cardList">
-                                        <label className="control-label col-6" htmlFor="comment">Card List:</label>
+                                        <label className="control-label col-6" htmlFor="cardList">Card List:</label>
                                         <div className="col-12">
-                                            <textarea className="form-control" placeholder={placeHolderText} id="comment"></textarea>
+                                            <textarea value={cardList} className="form-control" placeholder={placeHolderText} name="cardList"></textarea>
                                         </div>
                                     </div>
                                     <div className="form-group submit">        
