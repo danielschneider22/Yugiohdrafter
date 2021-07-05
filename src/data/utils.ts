1df21db844e1788a02fcd4ff5bdce167a3ae3b6d
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-07-05 11:29:21 -0400

    fix try catch

--- src/data/utils.ts
+++ src/data/utils.ts
@@ -14,7 +14,7 @@ export function stateAddItemWithoutMutation<T extends StateItem>(state: State<T>
 
 export const tryCatchPromise = (funcArgs?: any[]) => async function tryCatchPromise<T>(func: Function): Promise<[T | null, any | null]> { // will need a way to pass args
   try {
-    const data = await funcArgs ? func(...funcArgs as any[]) : func(funcArgs)
+    const data = await (funcArgs ? func(...funcArgs as any[]) : func(funcArgs))
     return [data, null]
   } catch (error: any) {
     return [null, error]
