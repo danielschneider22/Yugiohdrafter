d44181fcc467a1ca8b9a3263799c0cb8dcb52ff3
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2021-12-29 16:27:49 -0500

    fix custom set

--- src/constants/emailkey.ts
+++ src/constants/emailkey.ts
@@ -1,5 +1,7 @@
-export default {
+const key = {
     USER_ID: `user_jpbvrQOjz6Idg8LNmqLXU`, //userID
     TEMPLATE_ID: `template_0d65pqb`,         //templateID
     SERVICE_ID: `service_pvyom19`
 }
+
+export default key
--- src/data/cards/operations.ts
+++ src/data/cards/operations.ts
@@ -1,7 +1,10 @@
+import _ from "lodash";
 import { Dispatch } from "react";
 import { Card } from "../../constants/Card";
+import { toastBGColorDict } from "../../constants/Toast";
 import { getJSONWithErrorHandling } from "../../helpers/errorHandling";
 import { updateCardIds } from "../cardSets/actions";
+import { addToast } from "../toasts/actions";
 import { addCards } from "./actions";
 
 export async function fetchCards(dispatch: Dispatch<any>, set_name: string) {
@@ -16,17 +19,56 @@ export async function fetchCards(dispatch: Dispatch<any>, set_name: string) {
     
 }
 
-export async function fetchCardsByName(dispatch: Dispatch<any>, names: string[], setId: string) {
-    const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + names.join("|"));
-    const cards = await getJSONWithErrorHandling(response, dispatch, "Please check card list", "Invalid Card Name")
+function separateIntoBlocks(names: string[]){
+    const blocks = []
+    let currBlock = ""
+    names.forEach((name) => {
+        if(currBlock.length + name.length + 1 > 1120){
+            blocks.push(currBlock)
+            currBlock = name
+        } else {
+            currBlock = currBlock === "" ? name : currBlock + "|" + name
+        }
+    })
+    blocks.push(currBlock)
+    return blocks
+}
 
-    if(cards) {
-        dispatch(addCards(cards.data as Card[]))
-        dispatch(updateCardIds(cards.data as Card[], setId))
-        localStorage.setItem(setId, JSON.stringify(cards.data))
-        return true
+function toastFailedCards(dispatch: Dispatch<any>, names: string[], fullCardList: Card[]) {
+    let missingNames = ""
+    names.forEach((name) => {
+        if(!fullCardList.some((card) => card.name === name)) {
+            missingNames = missingNames + " " + name
+        }
+    })
+    if(missingNames) {
+        console.warn("Missing Cards: " + missingNames)
+        dispatch(addToast({id: _.uniqueId("error-"), type: "Danger", description: missingNames, title: "Cards not Found", backgroundColor: toastBGColorDict["Danger"]}))
     }
-    return false
+}
+function removeSpecialChars(names: string[]){
+    return names.map((name) => name.replace("#", "%23"))
+}
+
+export async function fetchCardsByName(dispatch: Dispatch<any>, names: string[], setId: string) {
+    const namesNoSpecialChars = removeSpecialChars(names)
+    const blocks = separateIntoBlocks(namesNoSpecialChars)
+    console.log(blocks)
+    const fullCardList: Card[] = []
+    for(const block of blocks) {
+        const response = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?name=' + block);
+        const cards = await getJSONWithErrorHandling(response, dispatch, "Please check card list", "Invalid Card Name")
+        if(cards) {
+            fullCardList.push(...cards.data)
+        } else {
+            return false
+        }
+    }
+    toastFailedCards(dispatch, names, fullCardList)
+    dispatch(addCards(fullCardList))
+    dispatch(updateCardIds(fullCardList, setId))
+    localStorage.setItem(setId, JSON.stringify(fullCardList))
+    return true
     
 }
 
