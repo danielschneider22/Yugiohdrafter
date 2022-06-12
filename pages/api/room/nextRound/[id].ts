8890ba3e65e6f29c1d594765185e730c86e64c54
danielschneider22 <60677431+danielschneider22@users.noreply.github.com>
2022-06-12 19:56:22 -0400

    next booster hosted game

---
+++ pages/api/room/nextRound/[id].ts
@@ -0,0 +1,26 @@
+import { WithId } from "mongodb";
+import { NextApiRequest, NextApiResponse } from "next";
+import { Booster } from "../../../../constants/Booster";
+import { Room } from "../../../../models/Room";
+import { connectToDatabase } from "../../../../mongodb";
+import { getRoom } from "../[id]";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  let { collections } = await connectToDatabase();
+  const roomId = (req.query.id as string)
+  const boostersNew: Booster[] = req.body.boostersDraft
+  let room = await collections.rooms?.findOne({id: roomId});
+  if (!room)
+    return res.status(500).json({ error: `Could not find room '.` })
+
+  await collections.boosters?.insertMany(boostersNew)
+
+  const boosterIdsLPIdx = room.boosterIdsLP.findIndex((id) => id === room!.currLPBoosterId)
+  const roomUpdates = {
+    currLPBoosterId: room.boosterIdsLP[boosterIdsLPIdx + 1],
+    boosterIdsDraft: boostersNew.map((booster) => booster.id),
+  }
+  collections.rooms?.updateOne({id: roomId}, {$set: roomUpdates });
+
+  return await getRoom((req.query.id as string), res)
+}
